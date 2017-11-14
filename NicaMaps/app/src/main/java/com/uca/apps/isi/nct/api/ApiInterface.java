37d668dd87c7b6f3fcad20d3b369bef4c180b99b
Ricky Bonilla <rdbr18lazio@gmail.com>
2017-11-13 23:16:12 -0600

    Agregué el fondo blanco

---
+++ NicaMaps/app/src/main/java/com/uca/apps/isi/nct/api/ApiInterface.java
@@ -0,0 +1,33 @@
+package com.uca.apps.isi.nct.api;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+import com.uca.apps.isi.nct.models.AccessToken;
+import com.uca.apps.isi.nct.models.Category;
+import com.uca.apps.isi.nct.models.Complaint;
+import com.uca.apps.isi.nct.models.User;
+
+public interface ApiInterface {
+
+    @GET("Categories")
+    Call<List<Category>> getCategories();
+
+    @GET("Complaints")
+    Call<List<Complaint>> getComplaints(@Header("Authorization") String authorization);
+
+    @POST("Complaints")
+    Call<Complaint> createComplaint(@Body Complaint complaint);
+
+    @POST("Users/login")
+    Call<AccessToken> login(@Body User user);
+
+    @POST("Users")
+    Call<User> signUp(@Body User user);
+}
